0525b022f426786fb633602dbd48434d483a47f0
Thomas Paine <tpaine@kineticsupply.com>
2014-10-16 13:53:00 -0500

    changed home date format

--- includes/views/home.php
+++ includes/views/home.php
@@ -1,6 +1,6 @@
 <?php render('_header',array('title'=>$title))?>
 
-<p>Welcome!  Today is <?php echo date('D d M Y')?></p>
+<p>Welcome!  Today is <?php echo date('D')?>, <?php echo date('M d')?>, <?php echo date('Y')?></p>
 
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
     <li data-role="list-divider">Choose a product category</li>
