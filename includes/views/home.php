047e3748b02fe34cc69d3c701b11f35ba29658ff
Thomas Paine <tpaine@kineticsupply.com>
2014-10-16 13:55:29 -0500

    changed home date format and intro text

--- includes/views/home.php
+++ includes/views/home.php
@@ -1,7 +1,7 @@
 <?php render('_header',array('title'=>$title))?>
-
-<p>Welcome!  Today is <?php echo date('D')?>, <?php echo date('M d')?>, <?php echo date('Y')?></p>
-
+<div style='text-align: center;'>
+    <p>Welcome!</br>Today is <?php echo date('d')?>-<?php echo date('M')?>-<?php echo date('Y')?></p>
+</div>
 <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
     <li data-role="list-divider">Choose a product category</li>
     <?php render($content) ?>
